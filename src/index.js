db99bdea62936cd48df3c42043208342a4d6e62c
Mauricio Maldonado <miturriago@uninorte.edu.co>
2021-03-25 16:23:40 -0500

    apollo config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  concat,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { createUploadLink } from "apollo-upload-client";
+
+const authLink = setContext(async (_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = await localStorage.getItem("token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? token : "",
+    },
+  };
+});
+
+const UploadClient = createUploadLink({
+  uri: "",
+});
+
+//client created
+global.client = new ApolloClient({
+  link: concat(authLink, UploadClient),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={global.client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
